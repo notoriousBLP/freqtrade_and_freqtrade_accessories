f74175bd1e07d3f91769b98c206debecbea1b9c1
Parncutt, Brandon <Brandon.Parncutt@nielsen.com>
2022-01-05 03:07:08 -0500

    fix indicator typo; added more roi settings; added trailing sell from uzichan strat

--- strategies/flawless_lambo.py
+++ strategies/flawless_lambo.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np  # noqa
 import pandas as pd  # noqa
 from pandas import DataFrame
@@ -7,8 +8,9 @@ import talib.abstract as ta
 import pandas_ta as pta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from freqtrade.exchange import timeframe_to_minutes
+from freqtrade.persistence import Trade
 from technical import indicators
-import logging
+from datetime import datetime, timezone
 from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter, RealParameter,
                                 IStrategy, IntParameter, merge_informative_pair)
 
@@ -789,7 +791,16 @@ class flawless_lambo(IStrategy):
             "3740": 0.00015599675850891392,
             "3745": 8.508914100486403e-05,
             "3750": 1.4181523500814142e-05,
-            "3755": 0
+            "3755": 0,
+            "3900": -0.01,
+            "3960": -0.02,
+            "4020": -0.03,
+            "4080": -0.04,
+            "4140": -0.05,
+            "4200": -0.06,
+            "4260": -0.07,
+            "4320": -0.08,
+            "4380": -0.09
         }
 
     # Optimal stoploss designed for the strategy.
@@ -820,6 +831,31 @@ class flawless_lambo(IStrategy):
     # hyperopt params
     sell_rsi = DecimalParameter(60, 100, default=73.9)
     sell_williams = DecimalParameter(-30, 0, default=-20)
+
+    # trailing sell (borrowed from UziChanTB2)
+    custom_info_trail_sell = dict()
+    trailing_sell_order_enabled = True    
+    # trailing_expire_seconds = 1800      #NOTE 5m timeframe
+    trailing_expire_seconds = 1800/5    #NOTE 1m timeframe
+    # trailing_expire_seconds = 1800*3    #NOTE 15m timeframe
+    trailing_sell_uptrend_enabled = True    
+    trailing_expire_seconds_uptrend = 90
+    min_uptrend_trailing_profit = 0.02
+    debug_mode = True
+    trailing_sell_max_stop = 0.02   # stop trailing sell if current_price < starting_price * (1+trailing_buy_max_stop)
+    trailing_sell_max_sell = 0.000  # sell if price between downlimit (=max of serie (current_price * (1 + trailing_sell_offset())) and (start_price * 1+trailing_sell_max_sell))
+    abort_trailing_when_sell_signal_triggered = True
+
+    
+    init_trailing_sell_dict = {
+        'trailing_sell_order_started': False,
+        'trailing_sell_order_downlimit': 0,        
+        'start_trailing_sell_price': 0,
+        'sell_tag': None,
+        'start_trailing_time': None,
+        'offset': 0,
+        'allow_sell_trailing': False,
+    }
 
     # Optional order type mapping.
     order_types = {
@@ -936,6 +972,91 @@ class flawless_lambo(IStrategy):
 
         return informative_pairs
 
+    def trailing_sell(self, pair, reinit=False):
+        # returns trailing sell info for pair (init if necessary)
+        if not pair in self.custom_info_trail_sell:
+            self.custom_info_trail_sell[pair] = dict()
+        if (reinit or not 'trailing_sell' in self.custom_info_trail_sell[pair]):
+            self.custom_info_trail_sell[pair]['trailing_sell'] = self.init_trailing_sell_dict.copy()
+        
+        return self.custom_info_trail_sell[pair]['trailing_sell']
+
+    def trailing_sell_info(self, pair: str, current_price: float):
+        # current_time live, dry run
+        current_time = datetime.now(timezone.utc)
+        if not self.debug_mode:
+            return
+        trailing_sell = self.trailing_sell(pair)
+
+        duration = 0
+        try:
+            duration = (current_time - trailing_sell['start_trailing_time'])
+        except TypeError:
+            duration = 0
+        finally:
+            self.logger.info("'\033[36m'SELL: "
+                f"pair: {pair} : "
+                f"start: {trailing_sell['start_trailing_sell_price']:.4f}, "
+                f"duration: {duration}, "
+                f"current: {current_price:.4f}, "
+                f"downlimit: {trailing_sell['trailing_sell_order_downlimit']:.4f}, "
+                f"profit: {self.current_trailing_sell_profit_ratio(pair, current_price)*100:.2f}%, "
+                f"offset: {trailing_sell['offset']}")
+
+    def current_trailing_sell_profit_ratio(self, pair: str, current_price: float) -> float:
+        trailing_sell = self.trailing_sell(pair)
+        if trailing_sell['trailing_sell_order_started']:
+            return (current_price - trailing_sell['start_trailing_sell_price'])/ trailing_sell['start_trailing_sell_price']
+            #return 0-((trailing_sell['start_trailing_sell_price'] - current_price) / trailing_sell['start_trailing_sell_price'])
+        else:
+            return 0
+    
+    def trailing_sell_offset(self, dataframe, pair: str, current_price: float):
+        # return rebound limit before a buy in % of initial price, function of current price
+        # return None to stop trailing buy (will start again at next buy signal)
+        # return 'forcebuy' to force immediate buy
+        # (example with 0.5%. initial price : 100 (uplimit is 100.5), 2nd price : 99 (no buy, uplimit updated to 99.5), 3price 98 (no buy uplimit updated to 98.5), 4th price 99 -> BUY
+        current_trailing_sell_profit_ratio = self.current_trailing_sell_profit_ratio(pair, current_price)
+        last_candle = dataframe.iloc[-1]
+        adapt  = (last_candle['perc_norm']).round(5)
+        default_offset = 0.003 * (1 + adapt)        #NOTE: default_offset 0.003 <--> 0.006
+        
+        trailing_sell  = self.trailing_sell(pair)
+        if not trailing_sell['trailing_sell_order_started']:
+            return default_offset
+
+        # example with duration and indicators
+        # dry run, live only
+        last_candle = dataframe.iloc[-1]
+        current_time = datetime.now(timezone.utc)
+        trailing_duration =  current_time - trailing_sell['start_trailing_time']
+        if trailing_duration.total_seconds() > self.trailing_expire_seconds:
+            if ((current_trailing_sell_profit_ratio > 0) and (last_candle['sell'] != 0)):
+                # more than 1h, price over first signal, sell signal still active -> sell
+                return 'forcesell'
+            else:
+                # wait for next signal
+                return None
+        elif (self.trailing_sell_uptrend_enabled and (trailing_duration.total_seconds() < self.trailing_expire_seconds_uptrend) and (current_trailing_sell_profit_ratio < (-1 * self.min_uptrend_trailing_profit))):
+            # less than 90s and price is falling, sell 
+            return 'forcesell'
+
+        if current_trailing_sell_profit_ratio > 0:
+            # current price is lower than initial price
+            return default_offset
+
+        trailing_sell_offset = {
+            # 0.06: 0.02,
+            # 0.03: 0.01,
+            0.1: default_offset,
+        }
+
+        for key in trailing_sell_offset:
+            if current_trailing_sell_profit_ratio < key:
+                return trailing_sell_offset[key]
+
+        return default_offset
+
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
@@ -998,14 +1119,14 @@ class flawless_lambo(IStrategy):
         # Stochastic Slow
         stoch_slow = ta.STOCH(dataframe)
         dataframe['slowd'] = stoch_slow['slowd']
-        dataframe['slowk'] = stoch_slow['slowd']
+        dataframe['slowk'] = stoch_slow['slowk']
 
+        self.trailing_sell(metadata['pair'])
 
         return dataframe
 
 
     def do_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         # Bollinger!
         bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
         dataframe['bb.lower'] = bollinger['lower']
@@ -1050,9 +1171,88 @@ class flawless_lambo(IStrategy):
         # Stochastic Slow
         stoch_slow = ta.STOCH(dataframe)
         dataframe['slowd'] = stoch_slow['slowd']
-        dataframe['slowk'] = stoch_slow['slowd']
+        dataframe['slowk'] = stoch_slow['slowk']
+
+        self.trailing_sell(metadata['pair'])
 
         return dataframe
+
+    def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
+                           rate: float, time_in_force: str, sell_reason: str, **kwargs) -> bool:
+        val = super().confirm_trade_exit(pair, trade, order_type, amount, rate, time_in_force, sell_reason, **kwargs)        
+        
+        if val:
+            if self.trailing_sell_order_enabled and self.config['runmode'].value in ('live', 'dry_run'):
+                val = False
+                dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+                if(len(dataframe) >= 1):
+                    last_candle = dataframe.iloc[-1].squeeze()
+                    current_price = rate
+                    trailing_sell= self.trailing_sell(pair)
+                    trailing_sell_offset = self.trailing_sell_offset(dataframe, pair, current_price)
+
+                    if trailing_sell['allow_sell_trailing']:
+                        if (not trailing_sell['trailing_sell_order_started'] and (last_candle['sell'] != 0)):
+                            trailing_sell['trailing_sell_order_started'] = True
+                            trailing_sell['trailing_sell_order_downlimit'] = last_candle['close']
+                            trailing_sell['start_trailing_sell_price'] = last_candle['close']
+                            trailing_sell['sell_tag'] = last_candle['sell_tag']
+                            trailing_sell['start_trailing_time'] = datetime.now(timezone.utc)
+                            trailing_sell['offset'] = 0
+                            
+                            self.trailing_sell_info(pair, current_price)
+                            self.logger.info(f'start trailing sell for {pair} at {last_candle["close"]}')
+
+                        elif trailing_sell['trailing_sell_order_started']:
+                            if trailing_sell_offset == 'forcesell':
+                                # sell in custom conditions
+                                val = True
+                                ratio = "%.2f" % ((self.current_trailing_sell_profit_ratio(pair, current_price)) * 100)
+                                self.trailing_sell_info(pair, current_price)
+                                self.logger.info(f"FORCESELL for {pair} ({ratio} %, {current_price})")
+
+                            elif trailing_sell_offset is None:
+                                # stop trailing sell custom conditions
+                                self.trailing_sell(pair, reinit=True)
+                                self.logger.info(f'STOP trailing sell for {pair} because "trailing sell offset" returned None')
+
+                            elif current_price > trailing_sell['trailing_sell_order_downlimit']:
+                                # update downlimit
+                                old_downlimit = trailing_sell["trailing_sell_order_downlimit"]
+                                self.custom_info_trail_sell[pair]['trailing_sell']['trailing_sell_order_downlimit'] = max(current_price * (1 - trailing_sell_offset), self.custom_info_trail_sell[pair]['trailing_sell']['trailing_sell_order_downlimit'])
+                                self.custom_info_trail_sell[pair]['trailing_sell']['offset'] = trailing_sell_offset
+                                self.trailing_sell_info(pair, current_price)
+                                self.logger.info(f'update trailing sell for {pair} at {old_downlimit} -> {self.custom_info_trail_sell[pair]["trailing_sell"]["trailing_sell_order_downlimit"]}')
+
+                            elif current_price > (trailing_sell['start_trailing_sell_price'] * (1 - self.trailing_sell_max_sell)):
+                                # sell! current price < downlimit && higher than starting price
+                                val = True
+                                ratio = "%.2f" % ((self.current_trailing_sell_profit_ratio(pair, current_price)) * 100)
+                                self.trailing_sell_info(pair, current_price)
+                                self.logger.info(f"current price ({current_price}) < downlimit ({trailing_sell['trailing_sell_order_downlimit']}) but higher than starting price ({(trailing_sell['start_trailing_sell_price'] * (1 + self.trailing_sell_max_sell))}). OK for {pair} ({ratio} %)")
+
+                            elif current_price < (trailing_sell['start_trailing_sell_price'] * (1 - self.trailing_sell_max_stop)):
+                                # stop trailing, sell fast, price too low
+                                val = True                                
+                                self.trailing_sell_info(pair, current_price)
+                                self.logger.info(f'STOP trailing sell for {pair} because of the price is much lower than starting price * {1 + self.trailing_sell_max_stop}')
+                            else:
+                                # uplimit > current_price > max_price, continue trailing and wait for the price to go down
+                                self.trailing_sell_info(pair, current_price)
+                                self.logger.info(f'price too low for {pair} !')
+
+                    else:
+                        self.logger.info(f"Wait for next sell signal for {pair}")
+
+                if (val == True):
+                    self.trailing_sell_info(pair, rate)
+                    self.trailing_sell(pair, reinit=True)
+                    self.logger.info(f'STOP trailing sell for {pair} because I SOLD it')
+
+        if sell_reason != 'sell_signal':
+            val = True
+
+        return val
         
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
@@ -1076,6 +1276,26 @@ class flawless_lambo(IStrategy):
             ),
             'sell'] = 1
 
+        if self.trailing_sell_order_enabled and self.config['runmode'].value in ('live', 'dry_run'): 
+            last_candle = dataframe.iloc[-1].squeeze()
+            trailing_sell = self.trailing_sell(metadata['pair'])
+            if (last_candle['sell'] != 0):
+                if not trailing_sell['trailing_sell_order_started']:
+                    open_trades = Trade.get_trades([Trade.pair == metadata['pair'], Trade.is_open.is_(True), ]).all()
+                    #if not open_trades: 
+                    if open_trades:
+                        self.logger.info(f"Set 'allow_SELL_trailing' to True for {metadata['pair']} to start *SELL* trailing")
+                        # self.custom_info_trail_buy[metadata['pair']]['trailing_buy']['allow_trailing'] = True
+                        trailing_sell['allow_sell_trailing'] = True
+                        initial_sell_tag = last_candle['sell_tag'] if 'sell_tag' in last_candle else 'sell signal'
+                        dataframe.loc[:, 'sell_tag'] = f"{initial_sell_tag} (start trail price {last_candle['close']})"
+            else:
+                if (trailing_sell['trailing_sell_order_started'] == True):
+                    self.logger.info(f"Continue trailing for {metadata['pair']}. Manually trigger sell signal!")
+                    dataframe.loc[:,'sell'] = 1
+                    dataframe.loc[:, 'sell_tag'] = trailing_sell['sell_tag']
+
         return dataframe
+
     
     # "All watched over by machines with loving grace..."
